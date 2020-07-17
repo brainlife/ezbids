bdc468a7196b9d39a35a3333daeb3d8e975a4dd2
Soichi Hayashi <hayashis@iu.edu>
2020-07-17 18:11:48 +0000

    renamed config.js to config.js.sample

--- api/config.js
+++ api/config.js
@@ -1,15 +1,1 @@
-'use strict';
-
-exports.mongodb = "mongodb://localhost/autobids";
-exports.mongoose_debug = true;
-
-//multer incoming upload directory
-exports.multer = {
-    dest: "/mnt/datalad/easybids/upload",
-}
-//directory to copy uploaded files (it needs to be on the same filesystem as multer incoming dir)
-exports.workdir = "/mnt/datalad/easybids/workdir",
-
-exports.express = {
-    port: 12507,
-}
+config.js.sample
