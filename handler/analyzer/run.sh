ba217dd1549c035dcc793dbc2b2cd5648804148d
Dan Levitas <djlevitas208@gmail.com>
2020-07-17 11:49:13 -0400

    Update run.sh

--- handler/analyzer/run.sh
+++ handler/analyzer/run.sh
@@ -6,6 +6,7 @@ set -x
 root=$1
 
 #this script should run dan's dicom directory analyzer
+python3 ezBIDS_data_extraction.py $root
 here=$( dirname "${BASH_SOURCE[0]}" )
 cp $here/ezbids_reference.json $root/ezbids.json
 
