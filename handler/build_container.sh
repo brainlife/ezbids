2cde61b9f9fd62801801c865a719e92fcde25830
Soichi Hayashi <hayashis@iu.edu>
2020-09-28 02:27:06 +0000

    added code to set EventName sidecar field

--- handler/build_container.sh
+++ handler/build_container.sh
@@ -2,5 +2,5 @@ set -x
 set -e
 
 docker build -t brainlife/ezbids-handler container
-docker tag brainlife/ezbids-handler brainlife/ezbids-handler:1.2
+docker tag brainlife/ezbids-handler brainlife/ezbids-handler:1.3
 docker push brainlife/ezbids-handler
