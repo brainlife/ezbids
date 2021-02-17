d08a5ef6167eed3ff01c5808d773d780b1e6d530
Dan Levitas <djlevitas208@gmail.com>
2021-02-17 09:54:01 -0500

    Add files via upload

---
+++ handler/deface.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 17 08:32:55 2021
+
+Reorient & deface anatomical images
+
+@author: dlevitas
+"""
+
+import os, sys json, deepdefacer
+import nibabel as nib
+
+root = sys.argv[0]
+
+finalize_json = open('{}/finalize.json'.format(root))
+finalize_json = json.load(finalize_json, strict=False)
+
+if finalize_json['defaced'] == True:
+    for i in range(len(finalize_json['objects'])):
+        if 'anat' in finalize_json['objects'][i]['_type']:
+            anat_path = [x for x in finalize_json['objects'][i]['paths'] if '.nii' in x][0]
+            
+            # anat_path = root + '/' + anat_path.split('./')[-1]
+            img = nib.load(anat_path)
+            new_img = nib.as_closest_canonical(img)
+            nib.save(new_img, anat_path)
+            
+            os.system('deepdefacer --input_file {} --defaced_output_path {}'.format(anat_path, anat_path.split('.nii')[0] + '_defaced' + '.nii.gz'))
+
