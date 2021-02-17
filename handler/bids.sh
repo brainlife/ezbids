63a05f94b9f4ec7119b864678346a67a3f0b7d4c
Dan Levitas <djlevitas208@gmail.com>
2021-02-17 09:54:42 -0500

    Update bids.sh

--- handler/bids.sh
+++ handler/bids.sh
@@ -11,30 +11,8 @@ root=$1
 
 rm -rf $root/bids
 
-echo "reorienting anatomical images to RAS+ and defacing"
-export root
-python_code=$(cat <<END
-import os, json, deepdefacer
-import nibabel as nib
-
-
-root = os.environ['root']
-finalize_json = open('{}/finalize.json'.format(root))
-finalize_json = json.load(finalize_json, strict=False)
-
-for i in range(len(finalize_json['objects'])):
-    if 'anat' in finalize_json['objects'][i]['_type']:
-        anat_path = [x for x in finalize_json['objects'][i]['paths'] if '.nii' in x][0]
-        
-        anat_path = root + '/' + anat_path.split('./')[-1]
-        img = nib.load(anat_path)
-        new_img = nib.as_closest_canonical(img)
-        nib.save(new_img, anat_path)
-        
-        os.system('deepdefacer --input_file {} --defaced_output_path {}'.format(anat_path, anat_path.split('.nii')[0] + '_defaced' + '.nii.gz'))
-END
-)
-run="$(python3 -c "$python_code")"
+echo "running defacing"
+./deface.py $root
 
 echo "converting output to bids"
 ./convert.js $root
