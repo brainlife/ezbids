02aa46603ef78dbf3a836c8e475614b77cfea568
Dan Levitas <djlevitas208@gmail.com>
2021-03-09 17:10:47 -0500

    Update deface.py

--- handler/deface.py
+++ handler/deface.py
@@ -2,49 +2,28 @@
 """
 Created on Wed Feb 17 08:32:55 2021
 
-Deface anatomical images
+Deface anatomical image(s)
 
 @author: dlevitas
 """
 
-import os, sys, json, time
+import os, sys
 import nibabel as nib
 import matplotlib.pyplot as plt
 plt.style.use('dark_background')
 from math import floor
-# from multiprocessing import Pool
 
 os.environ[ 'MPLCONFIGDIR' ] = '/tmp/'
 
-start = time.time()
-
 root = sys.argv[1]
-
-# if not os.path.isfile('{}/deface.out'.format(root)):
-#     os.system('touch {}/deface.out'.format(root))
-# log_file = '{}/deface.out'.format(root)
-
-finalized_json = open('{}/finalized.json'.format(root))
-finalized_json = json.load(finalized_json, strict=False)
-
-deface_list = []
-if finalized_json['deface'] == True:
-    for i in range(len(finalized_json['objects'])):
-        if 'anat' in finalized_json['objects'][i]['_type'] and finalized_json['objects'][i]['include'] == True:
-            sub = finalized_json['objects'][i]['_entities']['subject']
-            ses = finalized_json['objects'][i]['_entities']['session']
-            br_type = finalized_json['objects'][i]['_type']
-            anat_orig = root + '/' + finalized_json['objects'][i]['paths'][-1].split('./')[-1]
-            deface_list.append([anat_orig, br_type, sub, ses])
+deface_info = sys.argv[2]
  
-print('deface list is : {}'.format([x[0] for x in deface_list]))
-
-# Functions
-def deface(deface_list):
-    anat_orig = deface_list[0]
-    br_type = deface_list[1]
-    sub = deface_list[2]
-    ses = deface_list[3]
+# Function
+def deface(deface_info):
+    anat_orig = deface_info[0]
+    br_type = deface_info[1]
+    sub = deface_info[2]
+    ses = deface_info[3]
     anat_mask = anat_orig.split('.nii.gz')[0] + '_mask.nii.gz'
     anat_defaced = anat_orig.split('.nii.gz')[0] + '_defaced.nii.gz'
     
@@ -75,19 +54,11 @@ def deface(deface_list):
     else:
         dic = {'id': 1, 'defaced': anat_defaced, 'defaced_thumb': anat_defaced.split(root)[-1].split('.nii.gz')[0] + '.png', 'info': 'type {} is defaced for sub-{}/ses-{}'.format(br_type, sub, ses)}
 
-    file = open(log_file, "w")
-    file.write(repr(dic) + "\n")
-    file.close()
+#     file = open(log_file, "a")
+#     file.write(repr(dic) + "\n")
+#     file.close()
     print("thumbnail {}".format(dic), file=sys.stdout)
     
-# def deface_parallel():
-#     pool = Pool(processes=len(deface_list))
-#     pool.map(deface, deface_list)
-    
 
-# if __name__ == '__main__':
-#     deface_parallel()
 
-finish = time.time()
 
-print('Total elapsed defacing time was {}'.format(finish-start), file=sys.stdout)
