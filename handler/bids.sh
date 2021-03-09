6d6fb087d022114c751a7d671f20725abeae56fe
Dan Levitas <djlevitas208@gmail.com>
2021-03-09 16:48:12 -0500

    Update bids.sh

--- handler/bids.sh
+++ handler/bids.sh
@@ -12,7 +12,15 @@ root=$1
 rm -rf $root/bids
 
 echo "running defacing"
-./deface.py $root
+# parallel --wd $root -j 6 d2n {}
+if [ ! -f $root/deface.out ]; then
+    touch $root/deface.out
+fi
+function deface{
+    ./deface.py $root >> deface.out
+}
+parallel -j 10 deface 
+# ./deface.py $root
 
 echo "converting output to bids"
 ./convert.js $root
