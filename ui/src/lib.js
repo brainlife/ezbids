4ccdfb9d478654d2da5544ebd47e6cc6c1fab53f
Dan Levitas <djlevitas208@gmail.com>
2021-03-31 15:05:28 -0400

    Add files via upload

--- ui/src/lib.js
+++ ui/src/lib.js
@@ -1,20 +1,7 @@
 
 exports.setIntendedFor = $root=>{
-    overallIndex = 0
-
-    function arrayRemove(arr, value) { 
-        return arr.filter(function(ele){ 
-            return ele != value; 
-        });
-    }
 
     $root.subjects.forEach(sub=>{
-
-        // localizerIndices = []
-        objectIndices = []
-        objectIndicesOrig = []
-        protocol = []
-        sectionIndices = []
 
         // Determine how many sections there are per subject (and session)
         sectionIDs = []
@@ -38,27 +25,19 @@ exports.setIntendedFor = $root=>{
             fmapDwiPEDs = []
             funcIndices = []
             funcPEDs = []
-            // localizerIndices = []
-            objectIndices = []
-            objectIndicesOrig = []
 
             // Go through objects list and get values
             $root.objects.forEach(object=>{
                 if (object._entities.subject == sub.subject) {
-                    protocol.push(object._SeriesDescription)
-                    objectIndices.push(object.idx - overallIndex)
-                    objectIndicesOrig.push(object.idx)
 
                     if (object.analysisResults.section_ID == section) {
                         
-                        // if (object.analysisResults.errors != null && object.analysisResults.errors[0].includes('localizer')) {
-                        //     localizerIndices.push(object.idx - overallIndex)
-                        // }
-                        if (object._type == 'func/bold' && object._exclude == false) {
+                        if (object._type == 'func/bold' && !object._exclude) {
                             funcIndices.push(object.idx)
                             funcPEDs.push(object.items[0].sidecar.PhaseEncodingDirection)
                         }
-                        if (object._type == 'dwi/dwi' && object._exclude == false) {
+
+                        if (object._type == 'dwi/dwi' && !object._exclude) {
                             object.items.forEach(item=>{
                                 if (item.name == 'json') {
                                     dwiIndices.push(object.idx)
@@ -67,17 +46,16 @@ exports.setIntendedFor = $root=>{
                             });
                         }
 
-                        if (object._type.includes('fmap') && object._exclude == false) {
+                        if (object._type.startsWith('fmap') && !object._exclude) {
                             if (object._type.includes('mag') || object._type.includes('phasediff')) {
-                                fmapMagPhasediffIndices.push(object.idx) }
-
-                            else if (object._type.includes('mag') || object._type.includes('phase1') || object._type.includes('phase2')) {
-                                fmapMagPhaseIndices.push(object.idx) }
-                            else {
+                                fmapMagPhasediffIndices.push(object.idx)
+                            } else if (object._type.includes('mag') || object._type.includes('phase1') || object._type.includes('phase2')) {
+                                fmapMagPhaseIndices.push(object.idx)
+                            } else {
                                 if (object.forType == 'func/bold') {
                                     fmapFuncIndices.push(object.idx)
-                                    fmapFuncPEDs.push(object.items[0].sidecar.PhaseEncodingDirection) }
-                                else {
+                                    fmapFuncPEDs.push(object.items[0].sidecar.PhaseEncodingDirection)
+                                } else {
                                     fmapDwiIndices.push(object.idx)
                                     fmapDwiPEDs.push(object.items[0].sidecar.PhaseEncodingDirection)
                                 }
@@ -96,7 +74,7 @@ exports.setIntendedFor = $root=>{
                     }
                 // Remove all spin-echo fmaps except for last two
                 } else if (fmapFuncIndices.length > 2) {
-                    bad = fmapFuncIndices.slice(0,-2)
+                    let bad = fmapFuncIndices.slice(-2)
                     for (const i of bad) {
                         $root.objects[i].exclude = true
                         $root_objects[i].errors = 'Multiple spin echo field map pairs detected in section; only selecting last pair for BIDS conversion. The other pair acquisition(s) in this section will not be included in the BIDS output'
@@ -123,7 +101,7 @@ exports.setIntendedFor = $root=>{
                     }
                 // Remove all magnitudes and phasediff except for last 3
                 } else if (fmapMagPhasediffIndices.length > 3) {
-                    bad = fmapMagPhasediffIndices.slice(0,-3)
+                    let bad = fmapMagPhasediffIndices.slice(-3)
                     for (const i of bad) {
                         $root.objects[i].exclude = true
                         $root.objects[i].errors = 'More than three magnitude/phasediff field map acquisitions found in section. Only selecting most recent three. Others will not be included in the BIDS output'
@@ -136,7 +114,7 @@ exports.setIntendedFor = $root=>{
                     }
                 // Remove all magnitudes and phases except for last 4
                 } else if (fmapMagPhaseIndices.length > 4) {
-                    bad = fmapMagPhaseIndices.slice(0,-4)
+                    let bad = fmapMagPhaseIndices.slice(-4)
                     for (const i of bad) {
                         $root.objects[i].exclude = true
                         $root.objects[i].errors = 'Multiple images sets of (2 magnitude, 2 phase) field map acquisitions found in section. Only selecting most recent set. Other(s) will not be included in the BIDS output'
@@ -169,14 +147,14 @@ exports.setIntendedFor = $root=>{
             }
 
             // For spin-echo field maps, do not include func/bold (or dwi/dwi) acquisitions where the PEDs don't match
-            intendedFor = funcIndices
+            let intendedFor = funcIndices
             if (funcPEDs.length > 0 && fmapFuncPEDs.length > 0) {
-                for (i = 0; i < funcIndices.length; i++) {
-                    if (!fmapFuncPEDs.includes(funcPEDs[i])) {
-                        intendedFor = arrayRemove(intendedFor, funcIndices[i])
-                        $root.objects[funcIndices[i]].IntendedFor = intendedFor
+                funcIndices.forEach((funcIndex, idx)=>{
+                    if (!fmapFuncPEDs.includes(funcPEDs[idx])) {
+                        intendedFor = intendedFor.filter(v=>v != funcIndex)
+                        $root.objects[funcIndex].IntendedFor = intendedFor
                     }
-                }
+                });
             }
 
             // Enter the IntendedFor fields for the fmaps
@@ -190,8 +168,5 @@ exports.setIntendedFor = $root=>{
                 $root.objects[i].IntendedFor = funcIndices
             }
         }
-
-        overallIndex = overallIndex + protocol.length
-
     });
 }
