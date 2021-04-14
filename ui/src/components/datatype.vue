2b1255395455f3b0971ee7c4866fb8adf18e8dd1
Soichi Hayashi <soichih@gmail.com>
2021-04-14 15:34:36 -0400

    overriding the dot color for exclude datatype to red

--- ui/src/components/datatype.vue
+++ ui/src/components/datatype.vue
@@ -35,8 +35,12 @@ export default {
         },
 
         color() {
+            if(this.type == "exclude") return "hsl(0, 100%, 40%)";
+
             const hash = this.type.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0); 
-            const numhash = Math.abs(hash+12)%360;
+            let numhash = Math.abs(hash+12)%360;
+
+            
             return "hsl("+(numhash%360)+", 50%, 55%)"; 
         },
     },
