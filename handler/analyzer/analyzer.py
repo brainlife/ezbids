70254759a4b03b565b3826f7368aabb67a8cec2b
dlevitas <dlevitas@iu.edu>
2021-04-21 22:26:54 -0400

    update

--- handler/analyzer/analyzer.py
+++ handler/analyzer/analyzer.py
@@ -23,8 +23,7 @@ from math import floor
 warnings.filterwarnings("ignore")
 os.environ[ 'MPLCONFIGDIR' ] = '/tmp/'
 
-# data_dir = sys.argv[1]
-data_dir = '/media/data/ezbids/dicoms/WML/session_test'
+data_dir = sys.argv[1]
 os.chdir(data_dir)
 
 
