1a4bb36b7185b92a35d2e4670d3d78904d587214
dlevitas <dlevitas@iu.edu>
2021-09-03 14:31:57 -0400

    Bug fix

--- handler/analyzer/analyzer.py
+++ handler/analyzer/analyzer.py
@@ -440,6 +440,7 @@ def generate_dataset_list(uploaded_files_list):
     # SeriesNumber, and json_path.
     dataset_list = sorted(dataset_list, key=itemgetter("subject",
                                                        "AcquisitionDate",
+                                                       "AcquisitionTime",
                                                        "SeriesNumber",
                                                        "json_path"))
 
