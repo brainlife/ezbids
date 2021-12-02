29acf685266825cca386891f2e253c1a3cc4cd62
Soichi Hayashi <soichih@gmail.com>
2021-12-01 19:04:35 -0500

    added niivue for object page

---
+++ ui/src/components/niivue.vue
@@ -0,0 +1,78 @@
+<template>
+<el-dialog v-model="open" :title="item?.path" width="70%" destroy-on-close center @close="close">
+    <canvas ref="canvas" class="canvas" height="500"/>
+</el-dialog>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue'
+import { mapGetters, } from 'vuex'
+
+// @ts-ignore
+import { Niivue } from '@niivue/niivue'
+const nv = new Niivue();
+
+export default defineComponent({
+    props: [ 'item' ],
+    data() {
+        return {
+            open: false,
+        }
+    },
+
+    mounted() {
+        if(this.item) this.load();
+    },
+
+    watch: {
+        item() {
+            if(this.item) this.load();
+        }
+    },
+
+    computed: {
+        ...mapGetters(['getURL']),
+    },
+
+    methods: {
+        load() {
+            this.open = true;
+            this.$nextTick(()=>{
+                console.log("canvas", this.$refs.canvas)
+                nv.attachToCanvas(this.$refs.canvas);
+                nv.loadVolumes([{
+                    url: this.getURL(this.item.path),
+                    volume: {hdr: null, img: null},
+                    colorMap: "gray",
+                    opacity: 1,
+                    visible: true,
+                }]);
+            })
+        },
+
+        close() {
+            this.open = false;
+            this.$emit("close");
+        }
+
+    },
+});
+
+</script>
+<style scoped>
+.datatype {
+    font-size: 90%;
+}
+.bull {
+    width: 8px;
+    height: 8px;
+    display: inline-block;
+    border-radius: 50%;
+    position: relative;
+    top: 3px;
+}
+.canvas{
+    width: 100%;
+}
+</style>
