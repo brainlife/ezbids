a6e036735f257e4777b69f068812b575aeea1c8b
Soichi Hayashi <hayashis@iu.edu>
2021-12-08 22:02:26 +0000

    updated find_dicomdir.py to check if there is at least one .dcm/.DCM file. also updated to look for DICOMDIR as well as dicomdir for directory based dicom structure

--- handler/find_dicomdir.py
+++ handler/find_dicomdir.py
@@ -5,25 +5,32 @@ import sys
 
 def find_leaf(dir):
     leaf=True
+    hasDicom=False
 
     #is it leaf?
     for x in os.listdir(dir):
         fulldir=os.path.join(dir, x)
         if os.path.isdir(fulldir):
-            #print(fulldir+" is directory")
             leaf=False
             continue
+
+        #does it contain any .dcmfile?
+        if x.lower().endswith(".dcm"):
+            hasDicom=True
 
     #if it contains DICOMDIR, treat it as leaf
     if not leaf:
         for x in os.listdir(dir):
-            if x == "DICOMDIR":
-                #print("dicomdir detected")
+            if x.lower() == "dicomdir":
                 leaf=True
+                hasDicom=True
 
     if leaf:
-        #print(dir+" is leaf - no sub directory")
-        print(dir)
+        if hasDicom:
+            print(dir)
+        else:
+            #it's leaf but no dicom files.. let's not run dcm2niix
+            pass
     else:
         #recurse to all child dirs
         for x in os.listdir(dir):
