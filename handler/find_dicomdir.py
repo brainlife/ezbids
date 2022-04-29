73d03ce06c1aefee369c527c5a0493ee0d8c6491
Soichi Hayashi <soichih@gmail.com>
2022-04-29 09:52:19 -0400

    added check for all nifti directory.. it's a bit more brittle than https://github.com/brainlife/ezbids/commit/df29a4f01a1aebad37ad54d552d9315d0aa70cb0#diff-b4182bdc13a24b7ba56949b82467cad5bca5e0de62b34a2a2653264a775527a2L17-L20

--- handler/find_dicomdir.py
+++ handler/find_dicomdir.py
@@ -6,12 +6,12 @@ import sys
 def find_leaf(dir):
     leaf=True
     hasDicom=True
-    # hasDicom=False
-
 
     #is it leaf?
     for x in os.listdir(dir):
         fulldir=os.path.join(dir, x)
+
+        #don't consider parent directory as leaf
         if os.path.isdir(fulldir):
             leaf=False
             continue
@@ -19,6 +19,18 @@ def find_leaf(dir):
         # #does it contain any .dcmfile?
         # if x.lower().endswith(".dcm") or x.lower().endswith(".ima") or x.lower().startswith("mr."):
         #     hasDicom=True
+
+    #don't consider a leaf directory with nothing but .nii or .nii.gz
+    #otherwise dcm2niix gets run on that directory and it will fail
+    #it could still fail if directory doesn't have any dcm
+    if leaf:
+        allNifty=True
+        for x in os.listdir(dir):
+            if not x.endswith(".nii") and not x.endswith(".nii.gz"):
+                allNifty=False
+                break
+        if allNifty:
+            hasDicom=False
 
     #if it contains DICOMDIR, treat it as leaf
     if not leaf:
