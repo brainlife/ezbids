2ab31b9d47e40678e85aa728b07a2af58cb33ab4
Anthony Galassi <28850131+bendhouseart@users.noreply.github.com>
2022-12-22 20:36:03 -0600

    updated run script for ./dev.sh
    - updated docker-compose to use --platform linux/amd64 to play nicer w/
    m1 mac
    - updated handler/Dockerfile to fewer layers and fixed issue w/ node
      install
    - added logic to use docker compose if docker-compose not found
    when executing ./dev.sh

--- dev.sh
+++ dev.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -ex
 
@@ -10,7 +10,11 @@ git submodule update --init --recursive
 mkdir -p /tmp/upload
 mkdir -p /tmp/workdir
 
-#docker-compose up --build
-docker-compose up
-
-
+# ok docker compose is now included in docker as an option for docker
+if [[ $(command -v docker-compose) ]]; then 
+    # if the older version is installed use the dash
+    docker-compose up
+else
+    # if the newer version is installed don't use the dash
+    docker compose up
+fi
