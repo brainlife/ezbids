8795b42dcc39ba21991c9261120fd121e0741626
Anthony Galassi <28850131+bendhouseart@users.noreply.github.com>
2023-04-26 13:29:13 -0400

    added in newest changes from handler/preprocess.sh
    Getting new pandas errors when reading in `list` file in ezBIDS
    working directory. read_csv does not like being passed '\n' as a
    delimeter. But, PET2BIDs is running and making output, so that's
    good.

--- handler/preprocess.sh
+++ handler/preprocess.sh
@@ -26,112 +26,193 @@ echo "replace file path that contains space"
 # find $root -depth -name "* *" -execdir rename 's/ /_/g' "{}" \;
 find $root -depth -name "*[ ()]*" -execdir rename 's/[ )]/_/g; s/\(//g' "{}" \;
 
-# If there are .nii files, compress them to .nii.gz
-echo "Making sure all NIfTI files are in .nii.gz format"
-touch $root/nii_files
-find $root -name "*.nii" > $root/nii_files
-[ -s $root/nii_files ] && gzip $(cat $root/nii_files)
+# check to see if uploaded data is a BIDS-compliant dataset
+echo "Running bids-validator to check BIDS compliance"
 
-echo "processing $root"
+# Check the ezBIDS uploaded directory to see if it's a BIDS-compliant root directory or contains one
+# Search a few levels down in case a sub-directory is actually the BIDS root (could happen based on how user stored their data)
+substring="dataset_description.json"
+maxdepth=1
 
-echo "finding dicom directories"
-./find_dicomdir.py $root > $root/dcm2niix.list
-cat $root/dcm2niix.list
+test_root=($(find $root -maxdepth $maxdepth -type f \( -name $substring \)))
 
-# There can be overlap between the dicom directories and the pet directories
-# we want to use dcm2niix4pet for the pet directories, and dcm2niix for the dicom directories
+bids="no"
 
-echo "PET2BIDS is installed, using dcm2niix4pet for PET directories found in root dir ${root}"
-function rundcm2niix4pet {
-    #note.. this function runs inside $root (by --wd $root)
+until [[ $maxdepth -gt 5 ]] # go five levels in to check for existence of BIDS-compliant data
+do
+    if [[ "$test_root" == *"$substring"* ]]; then
+        bids="yes"
+        break
+    else
+        ((maxdepth++))
+        test_root=($(find $root -maxdepth $maxdepth -type f \( -name $substring \)))
+    fi
+done
 
-    path=$1
+if [[ $bids == "yes" ]]; then
+    test_root=${test_root///$substring/}
+else
+    test_root=$root
+fi
 
-    echo "----------------------- $path ------------------------"
-    timeout 3600 dcm2niix4pet $path
+if [ -f $test_root/.bidsignore]; then
+    touch $test_root/.bidsignore
+    echo "*finalized.json" > $test_root/.bidsignore
+else
+    echo "*finalized.json" >> $test_root/.bidsignore
+fi
 
-    #all good
-    echo $path >> pet2bids.done
-    PET2BIDS_RUN=true
-} 
+echo "*dcm2niix*" >> $test_root/.bidsignore
+echo "*preprocess*" >> $test_root/.bidsignore
+echo "*list" >> $test_root/.bidsignore
+echo "*nii_files" >> $test_root/.bidsignore
+echo "*ezBIDS_core.json" >> $test_root/.bidsignore
+echo "*bids_compliant.log" >> $test_root/.bidsignore
+echo "*validator.log" >> $test_root/.bidsignore
+echo "*.png" >> $test_root/.bidsignore
 
-export -f rundcm2niix4pet
+bids-validator $test_root > $test_root/validator.log || true
 
-# now we do a little magic to run pet2bids if it's installed
-./find_petdir.py $root > $root/pet2bids.list
-echo "Found PET directories:"
-cat $root/pet2bids.list
+if grep -w "ERR" $test_root/validator.log; then
+	echo "Uploaded data is not a BIDS-compliant dataset"
+    bids_compliant="no"
+else
+    echo "Uploaded data is a BIDS-compliant dataset"
+	bids_compliant="yes"
+fi
 
-# remove pet directories from dcm2niix list
-echo "Removing PET directories from dcm2niix list"
-echo "comm -13 ${root}/pet2bids.list ${root}/dcm2niix.list"
-comm -13 ${root}/pet2bids.list ${root}/dcm2niix.list
+echo $test_root > $root/bids_compliant.log
+echo $bids_compliant >> $root/bids_compliant.log
 
-# run pet2bids
-true > $root/pet2bids.done
+cat $root/bids_compliant.log
 
-cat $root/pet2bids.list | parallel --linebuffer --wd $root -j 6 rundcm2niix4pet {} 2>> $root/pet2bids_output
+if [ $bids_compliant == "yes" ]; then
+    # Skip certain processing steps, since uploaded data is already BIDS-compliant
 
-echo "running dcm2niix"
-true > $root/dcm2niix.done
-function d2n {
-    #note.. this function runs inside $root (by --wd $root)
+    touch $root/dcm2niix_output
+    touch $root/dcm2niix_error
+    
+    #find products
+    (cd $root && find . -mindepth 2 -type f \( -name "*.json" \) > list)
+    # remove irrelevant json files (ezBIDS_core.json, dataset_description.json, participants.json) if found
+    grep -F -v ezBIDS_core.json $root/list > $root/list_tmp && mv $root/list_tmp $root/list
+    grep -F -v dataset_description.json $root/list > $root/list_tmp && mv $root/list_tmp $root/list
+    grep -F -v participants.json $root/list > $root/list_tmp && mv $root/list_tmp $root/list
 
-    #set -e #we can't set this here because dcm2niix could return code:2
-    #which just means there are no DICOM files in that directory
-    set -x
+    echo "running ezBIDS_core (may take several minutes, depending on size of data)"
+    python3 "./ezBIDS_core/ezBIDS_core.py" $root
+else
 
-    path=$1
+    # If there are .nii files, compress them to .nii.gz
+    echo "Making sure all NIfTI files are in .nii.gz format"
+    touch $root/nii_files
+    find $root -name "*.nii" > $root/nii_files
+    [ -s $root/nii_files ] && gzip $(cat $root/nii_files)
 
-    # if path is empty then do nothing
-    if [ -z "$path" ]; then
-        echo "No path ${path} provided to d2n, skipping"
-    else 
+    echo "processing $root"
+
+    echo "finding dicom directories"
+    ./find_dicomdir.py $root > $root/dcm2niix.list
+    cat $root/dcm2niix.list
+
+    # There can be overlap between the dicom directories and the pet directories
+    # we want to use dcm2niix4pet for the pet directories, and dcm2niix for the dicom directories
+
+    echo "PET2BIDS is installed, using dcm2niix4pet for PET directories found in root dir ${root}"
+    function rundcm2niix4pet {
+        #note.. this function runs inside $root (by --wd $root)
+
+        path=$1
+
         echo "----------------------- $path ------------------------"
-        timeout 3600 dcm2niix --progress y -v 1 -ba n -z o -d 9 -f 'time-%t-sn-%s' $path
+        timeout 3600 dcm2niix4pet $path
 
         #all good
-        echo $path >> dcm2niix.done
-        DCM2NIIX_RUN=true
+        echo $path >> pet2bids.done
+        PET2BIDS_RUN=true
+    } 
+
+    export -f rundcm2niix4pet
+
+    # determine which uploaded files/folders are PET directories
+    ./find_petdir.py $root > $root/pet2bids.list
+    echo "Found PET directories:"
+    cat $root/pet2bids.list
+
+    # remove pet directories from dcm2niix list
+    echo "Removing PET directories from dcm2niix list"
+    echo "comm -13 ${root}/pet2bids.list ${root}/dcm2niix.list"
+    comm -13 ${root}/pet2bids.list ${root}/dcm2niix.list
+
+    # run pet2bids
+    true > $root/pet2bids.done
+
+    cat $root/pet2bids.list | parallel --linebuffer --wd $root -j 6 rundcm2niix4pet {} 2>> $root/pet2bids_output
+
+    echo "running dcm2niix"
+    true > $root/dcm2niix.done
+    function d2n {
+        #note.. this function runs inside $root (by --wd $root)
+
+        #set -e #we can't set this here because dcm2niix could return code:2
+        #which just means there are no DICOM files in that directory
+        set -x
+
+        path=$1
+
+        # if path is empty then do nothing
+        if [ -z "$path" ]; then
+            echo "No path ${path} provided to d2n, skipping"
+        else 
+            echo "----------------------- $path ------------------------"
+            timeout 3600 dcm2niix --progress y -v 1 -ba n -z o -d 9 -f 'time-%t-sn-%s' $path
+
+            #all good
+            echo $path >> dcm2niix.done
+            DCM2NIIX_RUN=true
+        fi
+    }
+
+    export -f d2n
+
+    cat $root/dcm2niix.list | parallel --linebuffer --wd $root -j 6 d2n {} 2>> $root/dcm2niix_output
+
+    #find products
+    echo "searching for products in $root"
+    (cd $root && find . -type f \( -name "*.json" \) > list)
+    # remove irrelevant json files (ezBIDS_core.json, dataset_description.json, participants.json) if found
+    grep -F -v ezBIDS_core.json $root/list > $root/list_tmp && mv $root/list_tmp $root/list
+    grep -F -v dataset_description.json $root/list > $root/list_tmp && mv $root/list_tmp $root/list
+    grep -F -v participants.json $root/list > $root/list_tmp && mv $root/list_tmp $root/list
+    
+    cat $root/list
+
+    if [ ! -s $root/list ]; then
+        echo "couldn't find any dicom files. aborting"
+        exit 1
     fi
-}
 
-export -f d2n
+    if [[ $DCM2NIIX_RUN -eq "true" ]]; then
+        # pull dcm2niix error information to log file
+        { grep -B 1 --group-separator=$'\n\n' Error $root/dcm2niix_output || true; } > $root/dcm2niix_error
+        # # remove error message(s) about not finding any DICOMs in folder
+        line_nums=$(grep -n 'Error: Unable to find any DICOM images' $root/dcm2niix_error | cut -d: -f1)
 
-cat $root/dcm2niix.list | parallel --linebuffer --wd $root -j 6 d2n {} 2>> $root/dcm2niix_output
+        for line_num in ${line_nums[*]}
+        do
+            sed -i "$((line_num-1)), $((line_num+1))d" $root/dcm2niix_error
+        done
+    fi
 
-#find products
-echo "searching for products in $root"
-(cd $root && find . -type f \( -name "*.json" \) > list)
-echo "echoing found products"
-echo "*******************************"
-cat $root/list
-echo "*******************************"
+    echo "running ezBIDS_core (may take several minutes, depending on size of data)"
+    python3 "./ezBIDS_core/ezBIDS_core.py" $root
 
-if [ ! -s $root/list ]; then
-    echo "couldn't find any dicom files. aborting"
-    exit 1
+    echo "generating thumbnails and movies for 3/4D acquisitions (may take several minutes, depending on size of data)"
+    cat $root/list | parallel --linebuffer -j 6 --progress python3 "./ezBIDS_core/createThumbnailsMovies.py" $root
+
+    echo "updating ezBIDS_core.json"
+    python3 "./ezBIDS_core/update_ezBIDS_core.py" $root
+
 fi
-
-if [[ $DCM2NIIX_RUN -eq "true" ]]; then
-    # pull dcm2niix error information to log file
-    { grep -B 1 --group-separator=$'\n\n' Error $root/dcm2niix_output || true; } > $root/dcm2niix_error
-    # # remove error message(s) about not finding any DICOMs in folder
-    line_nums=$(grep -n 'Error: Unable to find any DICOM images' $root/dcm2niix_error | cut -d: -f1)
-
-    for line_num in ${line_nums[*]}
-    do
-        sed -i "$((line_num-1)), $((line_num+1))d" $root/dcm2niix_error
-    done
-fi
-
-echo "running ezBIDS_core (may take several minutes, depending on size of data)"
-python3 "./ezBIDS_core/ezBIDS_core.py" $root
-
-echo "generating thumbnails and movies for 3/4D acquisitions (may take several minutes, depending on size of data)"
-cat $root/list | parallel --linebuffer -j 6 --progress python3 "./ezBIDS_core/createThumbnailsMovies.py" $root
-
-echo "updating ezBIDS_core.json"
-python3 "./ezBIDS_core/update_ezBIDS_core.py" $root
 
 echo "done preprocessing"
