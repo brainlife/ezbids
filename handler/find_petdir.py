09d5ec591ee6371906daa3ed95343f3d626da677
Anthony Galassi <28850131+bendhouseart@users.noreply.github.com>
2023-05-02 11:01:19 -0400

    switch find_petdir.py over to python calls instead of subprocess

--- handler/find_petdir.py
+++ handler/find_petdir.py
@@ -3,11 +3,13 @@
 import os
 import sys
 import subprocess
+from pathlib import Path
 
 # if pet2bids is installed we use it wherever PET dicoms live
 try:
     import pypet2bids
     pet2bidsInstalled = True
+    from pypet2bids import is_pet
 except (ImportError, ModuleNotFoundError):
     pet2bidsInstalled = False
     print(f"pet2bids is not installed, using dcm2niix on PET directories instead")
@@ -15,10 +17,10 @@ except (ImportError, ModuleNotFoundError):
 
 # change to input directory
 os.chdir(sys.argv[1])
-pet_folders_subprocess = subprocess.run(['ispet', '.'], capture_output=True, text=True)
+pet_folders = [str(folder) for folder in is_pet.pet_folder(Path(sys.argv[1]).resolve())]
 
 # parse output of ispet into list of directories
-pet_folders = [os.path.relpath(x, sys.argv[1]) for x in pet_folders_subprocess.stdout.splitlines() if x != '']
+pet_folders = [os.path.relpath(x, sys.argv[1]) for x in pet_folders if x != '']
 
 # format from expanded paths to relative paths to match output of find_dicomdir.py
 pet_folders = [os.path.join('.', x) for x in pet_folders]
@@ -28,4 +30,7 @@ if pet_folders:
     for pet_folder in pet_folders:
         print(pet_folder)
 
-sys.exit(pet_folders_subprocess.returncode)
+if pet_folders != []:
+    sys.exit(0)
+else:
+    sys.exit(1)
