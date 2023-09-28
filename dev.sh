26bc56dc6105759b81e1908bdc478ae5ecbcc814
Anibal <anibalsolon@gmail.com>
2023-09-28 14:37:03 -0500

    eh: add proxy to docker compose command

--- dev.sh
+++ dev.sh
@@ -4,6 +4,12 @@ set -ex
 
 git submodule update --init --recursive
 
+CMD=$@
+if [ -z "$CMD" ];
+then
+    CMD="up";
+fi
+
 # export UID=$(id -u)
 export UID
 export GID=$(id -g)
@@ -11,8 +17,8 @@ export GID=$(id -g)
 # ok docker compose is now included in docker as an option for docker
 if [[ $(command -v docker-compose) ]]; then 
     # if the older version is installed use the dash
-    docker-compose up
+    docker-compose $CMD
 else
     # if the newer version is installed don't use the dash
-    docker compose up
+    docker compose $CMD
 fi
