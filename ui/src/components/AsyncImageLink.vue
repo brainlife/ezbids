4a5770b60af1bccb01fbc75b6de68d034c14a6ef
Dan Levitas <dlevitas@iu.edu>
2023-11-27 22:47:36 +0000

    more sytax changes

--- ui/src/components/AsyncImageLink.vue
+++ ui/src/components/AsyncImageLink.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
-        <a style="cursor: pointer;" @click="handleGoToLink">
-            <img style="width: 100%" v-if="pathWithToken" :src="pathWithToken" alt="...loading" />
+        <a style="cursor: pointer" @click="handleGoToLink">
+            <img v-if="pathWithToken" style="width: 100%" :src="pathWithToken" alt="...loading" />
         </a>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from 'vue';
 import { mapState } from 'vuex';
 import axios from '../axios.instance';
 
@@ -17,37 +17,40 @@ export default defineComponent({
     },
     data() {
         return {
-            pathWithToken: ''
-        }
+            pathWithToken: '',
+        };
     },
     computed: {
-        ...mapState([ 'config', 'session' ]),
+        ...mapState(['config', 'session']),
     },
     watch: {
-        path: function(oldPath, newPath) {
+        path: function (oldPath, newPath) {
             this.updatePathWithToken();
-        }
+        },
+    },
+    mounted() {
+        this.updatePathWithToken();
     },
     methods: {
         handleGoToLink() {
             axios.get<string>(`${this.config.apihost}/download/${this.session._id}/token`).then((res) => {
-                window.location.href = `${this.getURL(this.path)}?token=${res.data}`
-            })
+                window.location.href = `${this.getURL(this.path)}?token=${res.data}`;
+            });
         },
         getURL(path: string | undefined) {
             if (!path) return '';
-            return `${this.config.apihost}/download/${this.session._id}/${path}`
+            return `${this.config.apihost}/download/${this.session._id}/${path}`;
         },
         updatePathWithToken() {
-            axios.get<string>(`${this.config.apihost}/download/${this.session._id}/token`).then((res) => {
-                this.pathWithToken = `${this.getURL(this.path)}?token=${res.data}`
-            }).catch((err) => {
-                console.error(err)
-            })
-        }
+            axios
+                .get<string>(`${this.config.apihost}/download/${this.session._id}/token`)
+                .then((res) => {
+                    this.pathWithToken = `${this.getURL(this.path)}?token=${res.data}`;
+                })
+                .catch((err) => {
+                    console.error(err);
+                });
+        },
     },
-    mounted() {
-        this.updatePathWithToken();
-    }
-})
+});
 </script>
