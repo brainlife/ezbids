8eb71429e3960a6d8e65f0f49586e8a1b37a1288
Dan Levitas <dlevitas@iu.edu>
2023-11-29 18:37:14 +0000

    singularity branch

---
+++ Singularity
@@ -0,0 +1,20 @@
+Bootstrap: docker
+From: node:16
+Stage: spython-base
+
+%files
+. /app
+%post
+
+
+mkdir -p /app
+cd /app
+npm install -g pm2 typescript tsc-watch
+
+npm install
+%runscript
+cd /app
+exec /bin/bash "$@"
+%startscript
+cd /app
+exec /bin/bash "$@"
--- dev.sh
+++ dev.sh
@@ -12,11 +12,19 @@ mkdir -p /tmp/workdir
 
 npm run prepare-husky
 
-# ok docker compose is now included in docker as an option for docker
-if [[ $(command -v docker-compose) ]]; then 
+# # ok docker compose is now included in docker as an option for docker
+# if [[ $(command -v docker-compose) ]]; then 
+#     # if the older version is installed use the dash
+#     docker-compose up
+# else
+#     # if the newer version is installed don't use the dash
+#     docker compose up
+# fi
+
+if [[ $(command -v singularity-compose) ]]; then 
     # if the older version is installed use the dash
-    docker-compose up
+    singularity-compose --debug up --no-resolv
 else
     # if the newer version is installed don't use the dash
-    docker compose up
+    singularity compose --debug up --no-resolv
 fi
