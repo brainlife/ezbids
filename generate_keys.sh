f357b03a5a0285e367e611795e52166bc6b76d46
Anibal <anibalsolon@gmail.com>
2024-01-18 13:51:20 -0600

    fix: key file name

--- generate_keys.sh
+++ generate_keys.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-if [ ! -f api/auth.key ]; then
+if [ ! -f api/ezbids.key ]; then
   openssl genrsa -out api/ezbids.key 2048
   chmod 600 api/ezbids.key
   openssl rsa -in api/ezbids.key -pubout > api/ezbids.pub
