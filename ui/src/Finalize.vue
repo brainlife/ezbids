3b9b9279ccf77b60f32ee257f6bd4878fcd56194
Dan Levitas <dlevitas@iu.edu>
2024-07-10 12:55:49 +0000

    update

--- ui/src/Finalize.vue
+++ ui/src/Finalize.vue
@@ -279,6 +279,7 @@ export default defineComponent({
                 const url = `${this.config.apihost}/download/${this.session._id}/bids/${this.ezbids.datasetDescription.Name}?token=${shortLivedJWT}`;
                 console.log(url)
 
+                window.location.href = url;
                 const fullurl = new URL(url, document.baseURI).href;
                 window.open('https://openneuro.org/import?url=' + encodeURI(fullurl));
 
