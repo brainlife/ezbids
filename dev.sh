9bcdcd72d084f3340c6b3c9e48e4eef978f9c20b
Anthony Galassi <28850131+bendhouseart@users.noreply.github.com>
2024-07-15 12:02:53 -0500

    added profile for telemetry while under development

--- dev.sh
+++ dev.sh
@@ -30,8 +30,8 @@ npm run prepare-husky
 # ok docker compose is now included in docker as an option for docker
 if [[ $(command -v docker-compose) ]]; then 
     # if the older version is installed use the dash
-    docker-compose up
+    docker-compose --profile development up
 else
     # if the newer version is installed don't use the dash
-    docker compose up
+    docker compose --profile development up
 fi
