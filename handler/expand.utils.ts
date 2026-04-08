b33d87162a2f59fe3b838f11f8b35835a25552f4
Nicholas Lee <niconal902@gmail.com>
2026-04-07 18:00:58 -0700

    fix: 7z force format

--- handler/expand.utils.ts
+++ handler/expand.utils.ts
@@ -3,6 +3,14 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { getBinPath } from './utils';
 
+function formatFor7z(basename: string): string[] {
+    if (basename.endsWith('.tar.gz') || basename.endsWith('.tgz')) return [];
+    if (basename.endsWith('.gz')) return ['-tgzip'];
+    if (basename.endsWith('.bz2')) return ['-tbzip2'];
+    if (basename.endsWith('.zst')) return ['-tzstd'];
+    return [];
+}
+
 export function walkDir(dir: string, files: string[]): void {
     const entries = fs.readdirSync(dir, { withFileTypes: true });
     for (const e of entries) {
@@ -34,7 +42,7 @@ export async function run7z(filePath: string, opts: Options, outDir?: string) {
     const arch = process.env.EZBIDS_ARCH;
     const sevenZName = platform === 'windows' ? `7z-${platform}-${arch}.exe` : `7z-${platform}-${arch}`;
     const sevenZPath = path.join(getBinPath('7z'), sevenZName);
-    const args = ['x', path.basename(filePath)];
+    const args = ['x', '-y', ...formatFor7z(path.basename(filePath)), path.basename(filePath)];
     if (outDir) args.push(`-o${outDir}`);
     try {
         await execa(sevenZPath, args, {
